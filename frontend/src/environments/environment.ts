8e3787e003034300c1a6aa89b6b5cefd1fc044c3
kitek420 <147552148+kitek420@users.noreply.github.com>
2025-06-28 21:34:32 +0200

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -10,6 +10,9 @@
 export const environment = {
   production: false,
   hostServer: 'http://localhost:3000'
+  nftAddress: '0x41427790c94E7a592B17ad694eD9c06A02bb9C39',
+  beeTokenAddress: '0x36435796Ca9be2bf150CE0dECc2D8Fab5C4d6E13',
+  beeFaucetAddress: '0x860e3616aD0E0dEDc23352891f3E10C4131EA5BC'
 }
 
 /*
